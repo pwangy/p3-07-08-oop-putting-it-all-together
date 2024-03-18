c4e0e2fd7792c12e09c62f7e853a2b94175d1227
pwangy <blipsandclicks@gmail.com>
2024-03-17 20:19:34 -0700

    complete lab

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,31 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
-        
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, title):
+        if not isinstance(title, str):
+            raise TypeError('Title must be a string.')
+        else:
+            self._title = title.title()
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, page_count):
+        if not isinstance(page_count, int):
+            print(f'page_count must be an integer')
+        else:
+            self._page_count = page_count
+
+    def turn_page(self):
+        print(f"Flipping the page...wow, you read fast!")
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,33 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+        self.condidtion = None
+
+    @property
+    def brand(self):
+        return self._brand
+
+    @brand.setter
+    def brand(self, brand):
+        if not isinstance(brand, str):
+            raise TypeError('Brand must be a string.')
+        else:
+            self._brand = brand.title()
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, size):
+        if not isinstance(size, int): #should be float?
+            print(f'size must be an integer')
+        else:
+            self._size = size
+
+    def cobble(self):
+        self.condition = "New"
+        print(f'Your shoe is as good as new!')
